e1c55177441510d12e81fff8f538dee3f617e613
Weston Platter <westonplatter@users.noreply.github.com>
2015-11-05 17:24:36 -0700

    Update columns based on import file.

--- sql/factual-places-create-table.sql
+++ sql/factual-places-create-table.sql
@@ -1,31 +1,31 @@
 
---create empty holding table
+/* create empty holding table */
+
 CREATE TABLE `factual_places` (
-  `factual_id` VARCHAR(36)  NOT NULL,
-  `name` VARCHAR(255)  NULL,
-  `address` VARCHAR(255) NULL,
-  `address_extended` VARCHAR(255) NULL,
-  `locality` VARCHAR(255)  NULL,
-  `region` VARCHAR(255)  NULL,
-  `postcode` VARCHAR(255)  NULL,
-  `country` VARCHAR(2)  NULL,
-  `neighborhood` TEXT  NULL,
-  `tel` VARCHAR(255)  NULL,
-  `fax` VARCHAR(255)  NULL,
-  `website` VARCHAR(255)  NULL,
-  `latitude` DOUBLE NULL ,
-  `longitude` DOUBLE NULL,
-  `chain_name` VARCHAR(255)  NULL,
-  `category_ids` VARCHAR(255)  NULL,
-  `post-town` VARCHAR(255)  NULL,
-  `chain_id` VARCHAR(36)  NULL,
-  `admin_region` VARCHAR(255)  NULL,
-  `po_box` VARCHAR(255)  NULL,
-  `hours_display` TEXT  NULL,
-  `email` VARCHAR(255)  NULL,
-  `category_labels` VARCHAR(255)  NULL
-
-)
-ENGINE = MyISAM;
-
-
+  `factual_id` varchar(36) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `address_extended` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `po_box` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `locality` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `region` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `post_town` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `admin_region` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `postcode` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `tel` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `fax` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `latitude` decimal(10,6) DEFAULT NULL,
+  `longitude` decimal(10,6) DEFAULT NULL,
+  `neighborhood` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `website` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `category_ids` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `category_labels` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `chain_id` int(11) DEFAULT NULL,
+  `chain_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `hours` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `hours_display` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `existence` decimal(2,2) DEFAULT NULL,
+   primary key(factual_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
